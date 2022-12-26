df8eaa86cf8becbdd9930237a322b31e9a4cb972
Miriam Castilhos <mncastilhos@ucs.br>
2022-12-25 21:22:51 -0300

    versionamento do código fonte construido no curso

---
+++ application/views/login.php
@@ -0,0 +1,45 @@
+<section style="min-height: calc(100vh - 83px);"class="light-bg">
+			<div class="container">
+				<div class="row">
+					<div class="col-offset-3 col-lg-6 text-center">
+						<div class="section-title">
+							<h2>LOGIN</h2>
+                            <form id="login_form" method="post">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <div class="imput-group">
+                                                <div class="input-group-addon">
+                                                    <span class="glyphicon glyphicon-user"></span>
+                                                </div> 
+                                                <input type="text" placeholder="Usuário" id="username" name="username" class="forme-control">
+                                            </div>    
+                                            <span class = "help-block"></span>
+                                        </div> 
+                                </div> 
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                            <div class="imput-group">
+                                                <div class="input-group-addon">
+                                                    <span class="glyphicon glyphicon-user"></span>
+                                                </div> 
+                                                <input type="passoword" placeholder="Senha" name="passoword" class="forme-control">
+                                            </div>    
+                                        </div> 
+                                </div> 
+
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <div class="form-group">
+                                             <button type="submit" id="btn-login" class="btn btn-block">Login</button>
+                                        </div> 
+                                     </div> 
+                                </div>                   
+                            </form>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- /.container -->
+		</section>
